87767fe752526f77eee8f2c9834554d561f2322c
Ryan Seit <ryan@ryanseit.com>
2020-02-01 20:02:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
+def fizzbuzz(entered_num)
+  if entered_num % 3 == 0 && entered_num % 5 == 0
     puts "FizzBuzz"
-  elsif number % 3 == 0
+  elsif entered_num % 3 == 0
     puts "Fizz"
-  elsif number % 5 == 0
+  elsif entered_num % 5 == 0
     puts "Buzz"
   else 
     puts nil
   end
 end
+
+fizzbuzz(3)
